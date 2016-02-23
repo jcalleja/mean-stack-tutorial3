03253005944753db6da9c3a86c20b96163516000
jcalleja <jcalleja@softstackfactory.org>
2016-02-23 01:22:05 +0000

    Push tutorial day 2

--- flapper-news/models/Comments.js
+++ flapper-news/models/Comments.js
@@ -4,7 +4,12 @@ var CommentSchema = new mongoose.Schema({
   body: String,
   author: String,
   upvotes: {type: Number, default: 0},
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+  comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
+
+CommentSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
 
 module.exports = mongoose.model('Comment', CommentSchema);
