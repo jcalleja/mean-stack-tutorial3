e3dc9f9b4c558bf3d437c4624449d67e8fcc9b68
jcalleja <jcalleja@softstackfactory.org>
2016-02-24 00:21:12 +0000

    Commit files

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -8,8 +8,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
+var passport = require('passport');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -28,6 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
